7025c869edf6b43546555b5a4fceb969f74a80ab
Salamandra19977 <salamandra199720@gmail.com>
2026-01-31 13:37:48 +0200

    3.0

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import style from './Main.module.scss'
 import AddForm from '../AddForm/AddForm'
+import MCalendar from '../MCalendar/MCalendar'
 import {FaRegCalendarPlus} from "react-icons/fa"
 
 function Main(props) {
   const [modalOpen, setModalOpen] = React.useState(false)
   return (
     <div className={style.wrapper}>
+      <MCalendar/>
       {modalOpen && <AddForm open={setModalOpen} />}
 
       <button 
